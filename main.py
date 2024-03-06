fabac0adba123086968ae1e7d712369a52b55e3c
dxia2p <d.xia111@gmail.com>
2024-03-06 09:59:30 -0700

    asdfa

---
+++ main.py
@@ -0,0 +1,10 @@
+class Backpack:
+    items = []
+    open = False
+    def __init__(self, color, size):
+        self.color = color
+        self.size = size
+    
+    def openBag():
+        print("BAG OPENED")
+        open = True
