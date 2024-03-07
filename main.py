f08b714286d46ea4b8b18e5864ee64b7a9ccb986
dxia2p <d.xia111@gmail.com>
2024-03-07 09:15:54 -0700

    asdfasd

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 class Backpack:
-    items = []
+    __items = []
     open = False
     def __init__(self, color, size):
         self.color = color
         self.size = size
     
-    def openBag():
+    def openBag(self):
         print("BAG OPENED")
-        open = True
+        self.open = True
+    def closeBag(self):
+        print("BAG CLOSED")
+        self.open = False
+    def putin(self, item):
+        if open:
+            self.__items.append(item)
+            print(f"{item} added to {self.color} {self.size} backpack")
+        else:
+            print("Backpack is not open")
+    def takeout(self, item):
+        if item in self.__items and self.open:
+            self.__items.remove(item)
+            print(f"{item} removed from {self.color} {self.size} backpack")
+        else:
+            print("Backpack is not open or item is not in backpack")
+
+smallBlueBackpack = Backpack("blue", "small")
+mediumRedBackpack = Backpack("red", "medium")
+largeGreenBackpack = Backpack("green", "large")
+
+backpacks = [smallBlueBackpack, mediumRedBackpack, largeGreenBackpack]
+for b in backpacks:
+    b.openBag()
+    b.putin("lunch")
+    b.putin("jacket")
+    b.closeBag()
+    b.openBag()
+    b.takeout("jacket")
+    b.closeBag()
